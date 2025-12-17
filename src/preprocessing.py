0c401226111c0a9b89ab54f20b3c8ec203f89cdf
ozgurangers <91689997+ozgurangers@users.noreply.github.com>
2025-12-17 15:17:39 +0300

    preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,18 @@
+import numpy as np
+from scipy.signal import butter, filtfilt
+
+def normalize_signal(signal):
+    return signal / np.max(np.abs(signal))
+
+def bandpass_filter(signal, fs, lowcut=100, highcut=2000, order=4):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return filtfilt(b, a, signal)
+
+def preprocess_signal(signal, fs):
+    signal = normalize_signal(signal)
+    signal = bandpass_filter(signal, fs)
+    return signal
 
